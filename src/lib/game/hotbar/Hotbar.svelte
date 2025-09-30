5b38b3304a16edbafa8bcf2cdf91606ba6f2b22c
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-09-30 10:24:17 -0400

    update hotbar ui

--- src/lib/game/hotbar/Hotbar.svelte
+++ src/lib/game/hotbar/Hotbar.svelte
@@ -7,35 +7,60 @@
 	}: {
 		mapManager: GameMapManager;
 	} = $props();
+
+	let hoveredName: string | undefined = $state(undefined);
 </script>
 
 <div class="wrap">
-	{#each gameBuildings as b (b)}
-		{@const building = new gameBuildingBehavior[b]()}
-		{@const buildingImage = building.getRenderer()}
-		<button
-			class="item"
-			onclick={(e) => {
-				e.preventDefault();
-				e.stopPropagation();
-				mapManager.setSelectedBuilding(building);
-			}}
-		>
-			<img src={buildingImage.src} alt={b} />
-		</button>
-	{/each}
+	{#if hoveredName}
+		{hoveredName}
+	{/if}
+	<div
+		class="buildings"
+		onmouseleave={() => {
+			hoveredName = undefined;
+		}}
+		role={null}
+	>
+		{#each gameBuildings as b (b)}
+			{@const building = new gameBuildingBehavior[b]()}
+			{@const buildingImage = building.getRenderer()}
+			<button
+				class="item"
+				onclick={(e) => {
+					e.preventDefault();
+					e.stopPropagation();
+					mapManager.setSelectedBuilding(building);
+				}}
+				onmouseenter={() => {
+					hoveredName = building.name;
+				}}
+			>
+				<img src={buildingImage.src} alt={b} />
+			</button>
+		{/each}
+	</div>
 </div>
 
 <style lang="scss">
 	.wrap {
 		position: fixed;
 		display: flex;
-		flex-direction: row;
+		flex-direction: column;
 		left: 50%;
 		bottom: 1rem;
+		transform: translate(-50%, 0);
+		color: var(--white);
+		align-items: center;
+		justify-content: center;
+	}
+
+	.buildings {
+		display: flex;
+		flex-direction: row;
+
 		border: 3px solid black;
 		border-right: 0px solid black;
-		transform: translate(-50%, 0);
 	}
 	.item {
 		all: unset;
@@ -48,5 +73,9 @@
 		display: flex;
 		align-items: center;
 		justify-content: center;
+
+		&:hover {
+			background: rgba(0, 0, 0, 0.6);
+		}
 	}
 </style>
